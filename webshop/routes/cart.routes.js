5361bfcf6ce75d53d09681c4f288349716677f8a
mp53586 <mp53586@fer.hr>
2023-06-04 20:49:58 +0200

    proboably done

---
+++ webshop/routes/cart.routes.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const session = require('express-session')
+
+const router = express.Router()
+const productData=require('../data/data');
+router.get("/cart/add/:id",(req,res)=>
+{var code=req.params.id;
+//update sessions
+if(req.session.cart)
+{req.session.cart;
+    var found=0;
+    for(var i=0;i<req.session.cart.length;i++)
+    {
+        if(req.session.cart[i].code==code)
+        {
+            req.session.cart[i].quantity=(req.session.cart[i].quantity+1);
+            found=1;
+            break;
+        }
+    }
+    req.session.totalProduct=(req.session.totalProduct+1);
+}
+else
+{
+}
+res.redirect(('/cart'));
+});
+router.get("/cart/remove/:id",(req,res)=>
+{var code=req.params.id;
+//update sessions
+if(req.session.cart)
+{req.session.cart;
+    var found=0;
+    for(var i=0;i<req.session.cart.length;i++)
+    {
+        if(req.session.cart[i].code==code)
+        {
+            req.session.cart[i].quantity=(req.session.cart[i].quantity-1);
+            found=1;
+             if(req.session.cart[i].quantity==0)
+             {
+               req.session.cart= req.session.cart.slice(0, i).concat(req.session.cart.slice(i+1));
+             }
+
+            break;
+        }
+    }
+    req.session.totalProduct=(req.session.totalProduct-1);
+}
+else
+{
+}
+res.redirect(('/cart'));
+});
--- webshop/routes/index.routes.js
+++ webshop/routes/index.routes.js
@@ -11,6 +11,55 @@ router.get("/cart",(req,res)=>
 {
     res.render('cart',{chosenCat:productData.categories.length-1,data:productData,total:req.session.totalProduct,cart:req.session.cart});
 });
+router.get("/cart/add/:id",(req,res)=>
+{var code=req.params.id;
+//update sessions
+if(req.session.cart)
+{req.session.cart;
+    var found=0;
+    for(var i=0;i<req.session.cart.length;i++)
+    {
+        if(req.session.cart[i].code==code)
+        {
+            req.session.cart[i].quantity=(req.session.cart[i].quantity+1);
+            found=1;
+            break;
+        }
+    }
+    req.session.totalProduct=(req.session.totalProduct+1);
+}
+else
+{
+}
+res.redirect(('/cart'));
+});
+router.get("/cart/remove/:id",(req,res)=>
+{var code=req.params.id;
+//update sessions
+if(req.session.cart)
+{req.session.cart;
+    var found=0;
+    for(var i=0;i<req.session.cart.length;i++)
+    {
+        if(req.session.cart[i].code==code)
+        {
+            req.session.cart[i].quantity=(req.session.cart[i].quantity-1);
+            found=1;
+             if(req.session.cart[i].quantity==0)
+             {
+               req.session.cart= req.session.cart.slice(0, i).concat(req.session.cart.slice(i+1));
+             }
+
+            break;
+        }
+    }
+    req.session.totalProduct=(req.session.totalProduct-1);
+}
+else
+{
+}
+res.redirect(('/cart'));
+});
 router.get("/getCategories",(req,res)=>
 {//this is useless
     //kategorije se moraju renderati svaki put kada se učitava index stranica, nema smisla ovaj dio odvajati od route za "/"
@@ -46,7 +95,6 @@ router.get("/getProducts/:id/add/:prodId",async (req,res)=>
     }
     else
     {
-        console.log("new sessss")
         var product ={code:code,quantity:1}
         req.session.cart=[product];
         req.session.totalProduct=1;
